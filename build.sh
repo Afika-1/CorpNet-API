25be68b2da0349c5480e10c226887d24ce246450
Que5 <phumlanibrightdlamini@gmail.com>
2025-06-05 19:01:51 +0200

    Added pip instal

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# Force pip to install dependencies here
+pip install -r requirements.txt
+
 # Navigate into your Django project directory
 cd social_media_api/
 
@@ -10,5 +13,5 @@ python manage.py collectstatic --noinput
 # Run database migrations
 python manage.py migrate
 
-# Go back to the root directory for the start command
-cd .. 
+# Go back to the root directory for the start command (THIS IS IMPORTANT FOR THE NEXT STEP'S GUNICORN COMMAND)
+cd ..
