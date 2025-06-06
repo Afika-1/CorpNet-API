e57dd7f420ce33a5df4ab61e1058297f26214015
Que5 <phumlanibrightdlamini@gmail.com>
2025-06-06 17:21:42 +0200

    Updated the build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# Force pip to install dependencies here
+pip install --upgrade pip
 pip install -r requirements.txt
-
-# Navigate into your Django project directory
 cd social_media_api/
-
-# Collect static files
 python manage.py collectstatic --noinput
-
-# Run database migrations
 python manage.py migrate
-
-# Go back to the root directory for the start command (THIS IS IMPORTANT FOR THE NEXT STEP'S GUNICORN COMMAND)
-# cd ..
