9df658beb8bbed74ff01b98b048f3891180a4079
Que5 <phumlanibrightdlamini@gmail.com>
2025-06-07 17:33:28 +0200

    Added the profiles on the admin page

--- social_media_api/accounts/admin.py
+++ social_media_api/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PersonalProfile, BusinessProfile, User
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(PersonalProfile)
+admin.site.register(BusinessProfile)
